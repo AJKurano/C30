5dcdca062d667a1bd1f51a9d8073459301bd7ba9
Anay Jaiswal <anayjaiswal2905@gmail.com>
2020-09-14 18:04:36 +0530

    Return Of The bird

--- Slingshot.js
+++ Slingshot.js
@@ -18,6 +18,10 @@ class SlingShot{
         this.sling.bodyA = null;
     }
 
+    attach(body){
+        this.sling.bodyA = body 
+    }
+
     display(){
         image(this.sling1,200,20);
         image(this.sling2,170,20);
--- sketch.js
+++ sketch.js
@@ -75,3 +75,10 @@ function mouseDragged(){
 function mouseReleased(){
     slingshot.fly();
 }
+
+
+function keyPressed(){
+    if(keyCode === 32){
+        slingshot.attach(bird.body)
+    }
+}
